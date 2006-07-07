da5c566dbe7aa1b2e33efee7d10876f744386be8
Grant Olson <kgo@grant-olson.net>
2006-07-07 20:15:02 +0000

    automated build

--- build.bat
+++ build.bat
@@ -1,3 +1,9 @@
+rmdir /S /Q build
+
+cvs -d:ext:grant@cvs.yoyodyne.gto:/usr/local/cvsroot export -dbuild -DNOW why_stackless
+
+cd build
+
 c:\Python24\python.exe c:\Python24\scripts\rst2html.py --stylesheet-path=style.css --embed-stylesheet why_stackless.txt why_stackless.html
 
 c:\Python24\python.exe c:\Python24\scripts\rst2mylatex.py why_stackless.txt --use-latex-toc --use-latex-docinfo --stylesheet=style.tex --use-verbatim-when-possible --documentclass=book --no-section-numbering --documentoptions=10pt,letter why_stackless.latex
@@ -10,3 +16,9 @@ dvipdfm why_stackless.dvi
 
 del why_stackless.aux why_stackless.dvi why_stackless.log why_stackless.out
 del why_stackless.latex
+del *.txt
+del build.bat style.css style.tex .cvsignore why_stackless.toc
+
+tar cvf code.tar code
+gzip code.tar
+rmdir /S /Q code
