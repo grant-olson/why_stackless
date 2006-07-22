b456c63355e96126a3c9de45bdfa19700b4436ef
Grant Olson <kgo@grant-olson.net>
2006-07-22 10:18:53 +0000

    Randome update

--- code/simpleMessageSocket.py
+++ code/simpleMessageSocket.py
@@ -4,19 +4,17 @@ from stacklesssocket import *
 def defaultMessageAction(msg):
     print "Received Message " ,msg
 
-def getMessages(act=defaultMessageAction):
+def getMessages(server,port, act=defaultMessageAction):
     listenSocket = socket(AF_INET, SOCK_STREAM)
     listenSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    listenSocket.bind(("127.0.0.1", 3000))
+    listenSocket.bind((server,port))
     listenSocket.listen(5)
     while 1:
         currentSocket, clientAddress = listenSocket.accept()
-        print "received connection from", clientAddress
         stackless.tasklet(processMessage)(currentSocket, clientAddress,act)
         stackless.schedule()
 
 def processMessage(sock,clientAddr,act=defaultMessageAction):
-    print "Processing msg from ", clientAddr
     msg = ""
     while 1:
         rec = sock.recv(32)
@@ -47,6 +45,6 @@ def sendMessage(server,port,msg):
         print "Got bad response"
 
 if __name__ == "__main__":
-    stackless.tasklet(getMessages)()
+    stackless.tasklet(getMessages)("127.0.0.1",3000)
     stackless.tasklet(sendMessage)( "127.0.0.1",3000,"this is a test")
     stackless.run()
